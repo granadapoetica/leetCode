3d2594acde5375743f4f9e6deb930b4c358883b2
Granada Poética! <58951809+granadapoetica@users.noreply.github.com>
2022-10-28 20:11:51 -0300

    Add files via upload

---
+++ ImplementacaoLeetCode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace ExerciciosLeetCode
+{
+    public static class ImplementacaoLeetCode
+    {
+        public static int[] TwoSum(int[] nums, int target){
+            // Ex: Nums: 2, 7, 11, 15 - Target: 9 
+            var arrSet = new Dictionary<int,int>();
+            int result = 0; 
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (arrSet.ContainsKey((nums[i])))
+                {
+                    if (arrSet.TryGetValue((nums[i]), out result))                    
+                        return new int[] { result, i };                    
+                }else{
+                    if(!arrSet.ContainsKey((target - nums[i]))) 
+                       arrSet.Add((target - nums[i]), i);
+                }
+            }
+            throw new System.Exception("Nenhum valor encontrado!"); 
+        } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExerciciosLeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
